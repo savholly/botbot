1752c24f1bdab8e48f35cb94f3e5a6380dc16536
sholly111 <sholly2@illinois.edu>
2022-09-26 22:57:39 -0500

    Fixed discord token, added .gitignore

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import commands
 import os
 import random
+from dotenv import load_dotenv
+load_dotenv('.env')
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='!', intents=intents)
 bot.videos = ['https://www.youtube.com/watch?v=V2qH7Rkzq24', 'https://www.youtube.com/watch?v=G92E3FWnHm0', 'https://www.youtube.com/watch?v=0DPZ9b9ZZr4']
@@ -33,4 +35,5 @@ async def calc(ctx, x: float, fn: str, y: float):
     await ctx.send(x / y)
   else:
     await ctx.send("We only support 4 function operations")
-bot.run('OTU5MjY2OTM4Mzc3MzY3NTUz.GbXRsp.JdyA15XRk93jroxfNtI_2VdWboID31m6DGqW0o')
+TOKEN = os.getenv('DISCORD_TOKEN')
+bot.run(TOKEN)
