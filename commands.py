893a33f6c890a8301eaae506ab868c7f248a511c
sholly111 <sholly2@illinois.edu>
2022-09-29 20:38:19 -0500

    added requirements.txt

--- commands.py
+++ commands.py
@@ -10,7 +10,7 @@ load_dotenv('.env')
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='!', intents=intents)
 bot.happylist = []
-@bot.command()
+@bot.command(name="hello", help="Greets the user who called the command")
 async def hello(ctx):
   await ctx.send("Hello " + ctx.author.display_name + "!")
 
@@ -75,6 +75,47 @@ async def coin(ctx):
 async def dice(ctx, n: int):
   num = random.randint(1, n)
   await ctx.send("You rolled a " + str(num) + "!")
+
+@bot.command()
+async def joke(ctx):
+  joke = get_joke()
+  await ctx.send(joke)
+
+@bot.command()
+async def meme(ctx):
+  meme = get_meme()
+  await ctx.send(meme)
+
+@bot.command()
+async def rockpaperscissors(ctx, rps: str):
+  rps = rps.lower()
+  rps_choices = ["rock", "paper", "scissors"]
+  bots_rps = random.choice(rps_choices)
+  if bots_rps == "rock":
+    if rps == "rock":
+      await ctx.send("Rock! It's a tie!")
+    elif rps == "paper":
+      await ctx.send("Rock! You win!")
+    elif rps == "scissors":
+      await ctx.send("Rock! You lose!")
+    else:
+      await ctx.send("You must choose rock, paper, or scissors! You lose!")
+  elif bots_rps == "paper":
+    if rps == "rock":
+      await ctx.send("Paper! You lose!")
+    elif rps == "paper":
+      await ctx.send("Paper! It's a tie!")
+    elif rps == "scissors":
+      await ctx.send("Paper! You win!")
+    else:
+      await ctx.send("You must choose rock, paper, or scissors! You lose!")
+  elif bots_rps == "scissors":
+    if rps == "rock":
+      await ctx.send("Scissors! You win!")
+    elif rps == "paper":
+      await ctx.send("Scissors! You lose!")
+    elif rps == "scissors":
+      await ctx.send("Scissors! It's a tie!")
 
 
 TOKEN = os.getenv('DISCORD_TOKEN')
--- helpers.py
+++ helpers.py
@@ -8,23 +8,17 @@ from dotenv import load_dotenv
 def get_cat():
     url = "https://api.thecatapi.com/v1/images/search"
     header={'x-api-key' : os.getenv('CAT_API_KEY')}
-    response = requests.get(url, header)
-    response = response.json()
-    response = response[0]["url"]
+    response = requests.get(url, header).json()[0]["url"]
     return (response)
 
 def get_dog():
     url = "https://dog.ceo/api/breeds/image/random"
-    response = requests.get(url)
-    response = response.json()
-    response = response["message"]
+    response = requests.get(url).json()["message"]
     return (response)
 
 def get_avery():
     url = "https://api.breakingbadquotes.xyz/v1/quotes"
-    response = requests.get(url)
-    response = response.json()
-    response = response[0]["quote"]
+    response = requests.get(url).json()[0]["quote"]
     return(response)
 
 def get_temp(q):
@@ -32,8 +26,7 @@ def get_temp(q):
     key = os.getenv('WEATHER_API_KEY')
     city = q
     url = url + "appid=" + key + "&q=" + city
-    response = requests.get(url)
-    response = response.json()
+    response = requests.get(url).json()
     temp_kelvin = response["main"]["temp"]
     temp_celc = temp_kelvin - 273
     temp_far = (1.8 * temp_celc) + 32
@@ -44,7 +37,17 @@ def get_condition(q):
     key = os.getenv('WEATHER_API_KEY')
     city = q
     url = url + "appid=" + key + "&q=" + city
-    response = requests.get(url)
-    response = response.json()
+    response = requests.get(url).json()
     condition = response["weather"][0]["main"]
     return(condition)
+
+def get_joke():
+    url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single"
+    response = requests.get(url).json()["joke"]
+    return(response)
+
+def get_meme():
+    url = "https://some-random-api.ml/meme"
+    response = requests.get(url).json()["image"]
+    return(response)
+
